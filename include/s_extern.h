2a963be06d04cd7d93452101a0f018568d2a7092
hsn <>
2003-10-04 12:56:32 +0000

    Initial revision

---
+++ include/s_extern.h
@@ -0,0 +1,89 @@
+#ifndef _FSP_S_EXTERN_H_
+#define _FSP_S_EXTERN_H_ 1
+
+/* conf.c, read and check configuration */
+extern int daemonize,use_prebuild_dirlists,read_only,dbug;
+extern int permit_passwordless_owners;
+extern int use_access_files,use_directory_mtime;
+extern int priv_mode,no_unnamed,logging,grab_enabled,ver_enabled;
+extern int homedir_restricted;
+extern uid_t run_uid;
+extern gid_t run_gid;
+extern unsigned int maxthcallowed;
+extern unsigned short udp_port,packetsize;
+extern time_t retry_timeout,session_timeout,stat_cache_timeout;
+extern char *logname,*readme_file,*dumpname;
+extern char *home_dir;
+extern unsigned int dir_cache_limit, stat_cache_limit,fp_cache_limit;
+extern char *tmp_dir;
+extern mode_t upload_umask, system_umask;
+void load_configuration PROTO1(const char *, conffile);
+void destroy_configuration PROTO0((void));
+
+/* file.c, file based disk i/o operations */
+int init_caches PROTO0((void));
+void shutdown_caches PROTO0((void));
+void stat_caches PROTO1(FILE *,fp);
+const char *validate_path PROTO0((char *, unsigned, PPATH *,DIRINFO **, int));
+const char *server_get_dir PROTO0((DIRLISTING **,const DIRINFO *));
+const char *server_del_file PROTO0((PPATH *, DIRINFO *));
+const char *server_del_dir PROTO2(PPATH *, pp, DIRINFO *,di);
+const char *server_make_dir PROTO0((PPATH *, unsigned long,DIRINFO **));
+const char *server_get_file PROTO0((PPATH *, FILE **, unsigned long,
+				     unsigned short,DIRINFO *));
+const char *server_get_pro PROTO3(DIRINFO *, di, char *, result, const char *, acc);
+const char *server_set_pro PROTO2(DIRINFO *,di, const char *, key);
+const char *server_up_load PROTO0((char *, unsigned int, unsigned long, unsigned long,
+				    unsigned short));
+const char *server_install PROTO0((PPATH *, unsigned long, unsigned short,const char *,DIRINFO *,unsigned int,const char *));
+const char *server_secure_file PROTO0((PPATH *, unsigned long,
+					unsigned short,DIRINFO *));
+const char *server_grab_file PROTO0((FILE **, unsigned long,
+				      unsigned short));
+const char *server_grab_done PROTO0((unsigned long, unsigned short));
+const char *server_stat PROTO1(UBUF *, buf);
+const char *server_rename PROTO0((char *,unsigned int,unsigned int));
+void init_home_dir PROTO0((void));
+
+/* filecache.c, open filehandles cache */
+
+/* iprange.c IP range services */
+extern IPrange *iptab;
+const char *check_ip_table PROTO2(unsigned long, inet_num,IPrange *,table);
+void free_ip_table PROTO1(IPrange *,table);
+void add_ipline PROTO2(const char *, text, IPrange **, table);
+void dump_iptab PROTO2(IPrange *,table,FILE *, fp);
+
+/* host.c, DNS and IP host databases */
+HTAB *find_host PROTO0((unsigned long));
+int init_htab PROTO0((void));
+int dump_htab PROTO1(FILE *,fn);
+
+/* server.c, network server operations */
+extern time_t cur_time;
+extern int shutdowning;
+RETSIGTYPE server_interrupt PROTO1(int, signum);
+RETSIGTYPE server_dump PROTO1(int, signum);
+int server_loop PROTO2(int,fd,time_t,timeout);
+int server_reply PROTO0((struct sockaddr_in *, UBUF *, unsigned int, unsigned int));
+void send_file PROTO0((struct sockaddr_in *, UBUF *, FILE *, unsigned int,
+			      char *));
+
+/* acl.c, security code */
+void load_access_rights PROTO1(DIRINFO *,di);
+void save_access_rights PROTO1(DIRINFO *,di);
+const char * require_access_rights PROTO4(const DIRINFO *,di,unsigned char,rights,unsigned long, ip_addr, const char *, passwd);
+
+/* main.c, startup and init code */
+
+/* log.c */
+extern int logfd;
+void fsplogf PROTO0((void));
+void fsplogs PROTO0((void));
+#ifdef __STDC__
+void fsploga(const char *fmt, ...);
+#else
+void fsploga(va_alist);
+#endif
+
+#endif /* _FSP_S_EXTERN_H_ */
---
+++ server/fifocache.c
@@ -0,0 +1,258 @@
+/* 
+ * Simple FIFO generic cache. (c) Radim Kolar 2003. 
+ * This file is copyrighted as LGPL.
+ *
+ * When this file is used as part of FSP, it uses 2-term BSD license
+ * (aka MIT/X11 License).
+ */
+
+#include "tweak.h"
+#include <malloc.h>
+#include <string.h>
+#include <stdio.h>
+#include "fifocache.h"
+
+/* allocates a memory for new cache stucture and init it */
+struct FifoCache * f_cache_new(unsigned int cachesize,unsigned int entrysize,
+    void (*edf) (void *),unsigned int keysize, void (*kdf) (void *), int (*kcf)(const void *,const void *))
+{
+    struct FifoCache *cache;
+    if(cachesize==0) return NULL;
+    if(entrysize==0 && keysize==0) return NULL;
+    cache=calloc(1,sizeof(struct FifoCache));
+    if(cache==NULL) return NULL;
+
+    cache->cachesize=cachesize;
+    cache->keysize=keysize;
+    cache->entrysize=entrysize;
+
+    if(entrysize==0)
+        cache->e_head=calloc(1,1);
+    else
+	cache->e_head=calloc(cachesize,entrysize);
+    
+    if(keysize==0)
+        cache->k_head=calloc(1,1);
+    else
+        cache->k_head=calloc(cachesize,keysize);
+
+    if(!cache->e_head || !cache->k_head)
+    {
+	f_cache_destroy(cache);
+	return NULL;
+    }
+    cache->k_next=cache->k_head;
+    
+    if(keysize==0)
+	cache->k_stop=NULL;
+    else
+	cache->k_stop=cache->k_head+cache->keysize*cache->cachesize;
+    
+    cache->e_next=cache->e_head;
+    if(entrysize==0)
+	cache->e_stop=NULL;
+    else
+	cache->e_stop=cache->e_head+cache->entrysize*cache->cachesize;
+    
+    cache->k_destroy_func=kdf;
+    cache->e_destroy_func=edf;
+    cache->k_compare_func=kcf;
+
+    return cache;
+}
+/* set memory profile functions */
+void f_cache_set_memory_profilers(struct FifoCache *cache,unsigned int (*keysize) (void *key),unsigned int (*entrysize) (void *entry))
+{
+    cache->get_keysize=keysize;
+    cache->get_entrysize=entrysize;
+}
+
+void f_cache_stats(struct FifoCache *cache,FILE *f)
+{
+    unsigned int i;
+    unsigned int used=0;
+    unsigned int j;
+    unsigned int dkey=0;
+    unsigned int dentry=0;
+
+    if(f==NULL) return;
+    /* count used items */
+    for(i=0;i<cache->cachesize;i++)
+    {
+        if(cache->keysize>0)
+	{
+	    for(j=0;j<cache->keysize;j++)
+	    {
+		if( *(cache->k_head+i*cache->keysize+j) != 0)
+		{
+		    used++;
+		    break;
+		}
+	    }
+	}
+    }
+
+    /* calculate dynamic memory used */
+    if(cache->get_keysize)
+    {
+	for(i=0;i<cache->cachesize;i++)
+	{
+	    dkey+=cache->get_keysize(cache->k_head+i*cache->keysize);
+	}
+    }
+    if(cache->get_entrysize)
+    {
+	for(i=0;i<cache->cachesize;i++)
+	{
+	    dentry+=cache->get_entrysize(cache->e_head+i*cache->entrysize);
+	}
+    }
+    fprintf(f,"%u entries (%u used). Memory: %u hdr, keys %u static + %u dynamic, entries %u static + %u dynamic. Hit ratio: %u hit, %u miss.\n",
+	    cache->cachesize,used,sizeof(struct FifoCache), cache->cachesize*cache->keysize,dkey,cache->cachesize*cache->entrysize,dentry,cache->hit,cache->miss);
+}
+
+unsigned int f_cache_void_profiler(void *anything)
+{
+    return 0;
+}
+
+/* destroys cache without deallocationg of elements itself */
+void f_cache_destroy(struct FifoCache *cache)
+{
+    if(cache==NULL) return;
+    if(cache->e_head) 
+    {
+	memset(cache->e_head,0,cache->entrysize*cache->cachesize);
+	free(cache->e_head);
+    }
+    if(cache->k_head) {
+	memset(cache->k_head,0,cache->keysize*cache->cachesize);
+	free(cache->k_head);
+    }
+    cache->e_head=NULL;
+    cache->k_head=NULL;
+    free(cache);
+}
+
+/* copy record into cache, free existing record */
+/* returns pointer to data entry in cache */
+void * f_cache_put(struct FifoCache *cache,const void *key,const void *data)
+{
+   void *where;
+   /* free place for new element */
+   if(cache->k_destroy_func) cache->k_destroy_func(cache->k_next);
+   if(cache->e_destroy_func) cache->e_destroy_func(cache->e_next);
+
+   /* copy new element in */
+   where=cache->e_next;
+   memcpy(cache->k_next,key,cache->keysize);
+   memcpy(cache->e_next,data,cache->entrysize);
+
+   /* update next pos. */
+   cache->e_next+=cache->entrysize;
+   cache->k_next+=cache->keysize;
+  
+   /* roll over? */
+   if(cache->e_next==cache->e_stop || cache->k_next==cache->k_stop)
+   {
+       cache->e_next=cache->e_head;
+       cache->k_next=cache->k_head;
+   }
+
+   return where;
+}
+
+/* find element by key */
+void *f_cache_find(struct FifoCache *cache,const void *key)
+{
+    unsigned int i;
+    
+    if(!cache->k_compare_func) return NULL;
+    if(cache->keysize==0) return NULL;
+   
+    for(i=0;i<cache->cachesize;i++)
+	if(!cache->k_compare_func(key,cache->k_head+i*cache->keysize))
+	{
+	    cache->hit++;
+	    return cache->e_head+i*cache->entrysize;
+	}
+    cache->miss++;
+    return NULL;
+}
+
+/* clear all elements from the cache */
+void f_cache_clear(struct FifoCache *cache)
+{
+    unsigned int i;
+
+    /* free entries */
+    for(i=0;i<cache->cachesize;i++)
+    {
+       if(cache->k_destroy_func) 
+	   cache->k_destroy_func(cache->k_head+i*cache->keysize);
+       if(cache->e_destroy_func) 
+	   cache->e_destroy_func(cache->e_head+i*cache->entrysize);
+    }
+
+    /* clear entries */
+    memset(cache->k_head,0,cache->cachesize*cache->keysize);
+    memset(cache->e_head,0,cache->cachesize*cache->entrysize);
+    
+    cache->k_next=cache->k_head;
+    cache->e_next=cache->e_head;
+
+    cache->hit=0;
+    cache->miss=0;
+}
+/* find key for given entry */
+void * f_cache_get_key(struct FifoCache *cache,const void *entry)
+{
+    unsigned int i;
+    if(cache->entrysize==0 || cache->keysize==0) return NULL;
+
+    /* check if pointer is good */
+    if(entry<(const void *)cache->e_head || entry>=(const void *)cache->e_stop) return NULL;
+    /* find cache index */
+    i=((const BYTE *)(entry)-cache->e_head)/cache->entrysize;
+    return cache->k_head+cache->keysize*i;
+}
+
+/* delete entry from cache, returns one if object is deleted */
+int f_cache_delete_entry(struct FifoCache *cache, void *entry)
+{
+    unsigned int i;
+
+    if(cache->entrysize==0) return 0;
+    /* check if pointer is good */
+    if(entry<(const void *)cache->e_head || entry>=(const void *)cache->e_stop) return 0;
+    /* find cache index */
+    i=((BYTE *)(entry)-cache->e_head)/cache->entrysize;
+    
+    /* deallocate */
+    if(cache->k_destroy_func) cache->k_destroy_func(cache->k_head+cache->keysize*i);
+    if(cache->e_destroy_func) cache->e_destroy_func(entry);
+    /* zero them */
+    memset(entry,0,cache->entrysize);
+    memset(cache->k_head+cache->keysize*i,0,cache->keysize);
+    
+    return 1;
+}
+
+/* returns how many objects was deleted */
+int f_cache_delete_by_key(struct FifoCache *cache, void *key)
+{
+    unsigned int i;
+    int deleted=0;
+
+    if(!cache->k_compare_func) return 0;
+
+    for(i=0;i<cache->cachesize;i++)
+    {
+	if(!cache->k_compare_func(key,cache->k_head+i*cache->keysize))
+	{
+	    deleted+=f_cache_delete_entry(cache,cache->e_head+i*cache->entrysize);
+	}
+    }
+
+    return deleted;
+}
