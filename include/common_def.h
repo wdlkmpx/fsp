f9d058498806c50deda704c754166c9118f7f8ec
hsn <>
2005-03-17 21:26:56 +0000

    *** empty log message ***

--- include/common_def.h
+++ include/common_def.h
@@ -54,7 +54,7 @@
 #define UBUF_HSIZE 12                           /* 12 bytes for the header */
 #define UBUF_SPACE 1024			        /* maximum standard payload. */
 #define UBUF_MAXSPACE 4096                      /* maximum payload supported by server */
-#define DEFAULT_SPACE 1370			/* Default packet size */
+#define DEFAULT_SPACE 1500-20-8-12		/* Max packet size for standard 1500 mtu ethernet */
 
 #define NBSIZE (UBUF_MAXSPACE+UBUF_SPACE)
 
