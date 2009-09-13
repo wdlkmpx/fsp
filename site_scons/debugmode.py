7da1c7fcf62b4e990bd8fc4caa0aad38644842f9
Radim Kolar <>
2009-09-13 20:12:03 +0200

    Debug mode returns True/False if debug build was requested

--- site_scons/debugmode.py
+++ site_scons/debugmode.py
@@ -1,8 +1,8 @@
 #
 # SCons debug mode build tester
 #
-# Version 1.1
-# 18-Aug-2009
+# Version 1.2
+# 13-Sep-2009
 #
 
 from SCons.Script import ARGUMENTS
@@ -17,6 +17,8 @@ def checkForDebugBuild(conf):
          buildlamer2=None
     if buildlamer2 == 0 or str(buildlamer).lower() == 'no':
         conf.Result(0)
+        return False
     else:
         conf.env.Append(CPPFLAGS = '-DDEBUG')
         conf.Result(1)
+        return True
