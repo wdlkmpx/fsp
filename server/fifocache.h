ac199f985f37a8a2143a6a783e48dbc2ca863849
Radim Kolar <>
2009-09-22 16:53:39 +0200

    use void * instead of BYTE pointers

--- server/fifocache.h
+++ server/fifocache.h
@@ -9,13 +9,13 @@
 struct FifoCache
 {
     unsigned int cachesize; /* max. number of entries */
-    BYTE *e_head; /* block of cache entries, every entry has entrysize bytes */
-    BYTE *e_next; /* new intem will be placed there */
-    const BYTE *e_stop; /* stop mark for entries, never write here! */
+    void *e_head; /* block of cache entries, every entry has entrysize bytes */
+    void *e_next; /* new intem will be placed there */
+    const void *e_stop; /* stop mark for entries, never write here! */
     unsigned int entrysize; /* size of 1 entry in bytes */
-    BYTE *k_head; /* block of keys starts there */
-    BYTE *k_next; /* new item */
-    const BYTE *k_stop; /* stop mark for key entries */
+    void *k_head; /* block of keys starts there */
+    void *k_next; /* new item */
+    const void *k_stop; /* stop mark for key entries */
     unsigned int keysize; /* size of 1 key in bytes */
     void (*k_destroy_func) (void *key); /* key destoy function */
     void (*e_destroy_func) (void *key); /* element destoy function */
