eea51abb1dd9b2e06c02a10da03449427263f3e2
Radim Kolar <hsn@sendmail.cz>
2019-08-16 14:11:55 +0200

    apply patch for fspscan from FreeBSD ports

--- contrib/fspscan.c
+++ contrib/fspscan.c
@@ -72,7 +72,7 @@ static void printhelp(int argc,char **argv)
     exit(0);
 }
 
-static void fdclose(void)
+static void myfdclose(void)
 {
     close(myfd);
 }
@@ -150,7 +150,7 @@ int main(int argc,char **argv)
         else
 	    fprintf(logfile,"%0d...nada\n",remoteport);
         fflush(logfile);
-        fdclose();
+        myfdclose();
     }
     return 0;
 }
