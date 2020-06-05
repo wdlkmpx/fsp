dc69111e84918ad4563dd88cb9a6215fcc726f27
Radim Kolar <hsn@sendmail.cz>
2020-06-05 17:19:48 +0200

    whitespace fix

--- include/client_def.h
+++ include/client_def.h
@@ -19,11 +19,11 @@
 * Added 11 Jan 93 by A.E.J.Fellows                                       *
 *************************************************************************/
 
-#define C_NOCLOBBER	1       /* Will not overwrite file of same name  */
-#define C_CLOBBER	2       /* Will always overwrite file even if
+#define C_NOCLOBBER	1	/* Will not overwrite file of same name  */
+#define C_CLOBBER	2	/* Will always overwrite file even if
                                    remote file is not found              */
-#define C_UNIQUE	3       /* Create unique name to avoid overwrite */
-#define C_TEMPNAME   	4       /* Download to temp name                 */
+#define C_UNIQUE	3	/* Create unique name to avoid overwrite */
+#define C_TEMPNAME	4	/* Download to temp name                 */
 #define C_APPEND	5	/* Downloads will attempt to append to
 				   end of file if it already exists      */
 
