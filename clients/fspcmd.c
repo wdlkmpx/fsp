295980ee234b2366bd555faa61fc3066bdad6add
wdlkmpx <wdlkmpx@gmail.com>
2021-11-01 17:27:56 -0500

    remove fspcmd_hostcmd.c and related files

--- clients/fspcmd.c
+++ clients/fspcmd.c
@@ -28,7 +28,6 @@ int frmcmd_main    (int argc, char ** argv);
 int frmdircmd_main (int argc, char ** argv);
 int fver_main      (int argc, char ** argv);
 int fducmd_main    (int argc, char ** argv);
-int fhostcmd_main  (int argc, char ** argv);
 int ffindcmd_main  (int argc, char ** argv);
 int fstatcmd_main  (int argc, char ** argv);
 int fmvcmd_main    (int argc, char ** argv);
@@ -41,7 +40,7 @@ static void fspcmd_usage (char *app)
 	printf ("Supported commands:\n");
 	printf ("   bye, cat, cd, du, get, grab\n");
 	printf ("   ls, mkdir, mv, pro, put, rm\n");
-	printf ("   rmdir, hostcmd, find, stat, ver\n\n");
+	printf ("   rmdir, find, stat, ver\n\n");
 }
 
 // ================================================
@@ -73,7 +72,6 @@ int main (int argc, char ** argv)
   else if (!strcmp(cmd,"put"))   fput_main(argc,argv);
   else if (!strcmp(cmd,"rm"))    frmcmd_main(argc,argv);
   else if (!strcmp(cmd,"rmdir")) frmdircmd_main(argc,argv);
-  else if (!strcmp(cmd,"hostcmd")) fhostcmd_main(argc,argv);
   else if (!strcmp(cmd,"find"))    ffindcmd_main(argc,argv);
   else if (!strcmp(cmd,"stat"))    fstatcmd_main(argc,argv);
   else if (!strcmp(cmd,"ver"))     fver_main(argc,argv);
